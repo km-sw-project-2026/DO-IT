8db4bcc353afd820aec52d335ffc56319edfea53
shinyubin1015 <penutljh@gmail.com>
2026-01-31 06:37:57 +0000

    feat: 게시물 등록

--- worker/index.js
+++ worker/index.js
@@ -5,9 +5,11 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/api/posts") {
+    if (request.method === "GET" && url.pathname === "/api/posts") {
       // /api/posts 경로로 요청이 들어오면 posts.onRequestGet 호출
-      return posts.onRequestGet({url, env});
+      return posts.onRequestGet({ url, env });
+    } else if (request.method === "POST" && url.pathname === "/api/posts") {
+      return posts.onRequestPost({ request, env });
     } else if (url.pathname.startsWith("/api/post/")) {
       // /api/post/:id 경로로 요청이 들어오면 ID 추출하여 postById.onRequestGet 호출
       const id = url.pathname.replace(/^\/api\/post\/+/, '');
