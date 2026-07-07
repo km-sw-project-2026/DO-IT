17c844de80d0f49711775598bd737aec00bd8726
shinyubin1015 <penutljh@gmail.com>
2026-07-07 11:35:57 +0000

    반응형7

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,10 +1,8 @@
-import LoginHeader from "./loginheader";
 import { Outlet } from "react-router-dom";
 
 export const UserLayout = () => {
     return (
         <>
-            <LoginHeader/>
             <Outlet/>
         </>
     );
